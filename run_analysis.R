311aab3b1fa48d15df31125f7ba1ba35691fcf67
Budd <tbudd@solers.com>
2015-11-18 12:24:21 -0500

    Tidy-ed data, calculated means, printed output Added codebook

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,20 @@
 run_analysis <- function(){
-  ## this function assumes all of the files are in .\UCI HAR Dataset in the original file heirarchy
+  ## This function computes the average of the mean() and std() variables in the UCI HAR dataset.
+  ## First it combines the subject and activity variables with each of the test and training tables.
+  ## Then it combines the test and training tables and extracts only the mean() and st() variables for averaging.
+  ## Finally, it averages the variables and prints the result to a file and returns the result as well
+  
+  ## This function assumes all of the files are in ".\UCI HAR Dataset" in the original file heirarchy
   ## For field and file descriptions refer to the run_analysis code book.
   
   ## necessary libraries
   library("dplyr")
 
-  ## Read in data files and name the columns human readibly
+  ## Read in activity labels; col act is the numeric index; activity is the text descriptor
   activ.labels<-tbl_df(read.table("UCI HAR Dataset\\activity_labels.txt",stringsAsFactors = FALSE))
   activ.labels<-select(activ.labels,act=V1,activity=V2)
   
+  ## Read in features; ndx is the index; name is the text descriptor
   features<-tbl_df(read.table("UCI HAR Dataset\\features.txt",stringsAsFactors = FALSE))
   features<-select(features,ndx=V1,name=V2)
 
@@ -27,12 +33,14 @@ run_analysis <- function(){
   ## Append the test observations to the training observations
   merged<-rbind(train,test)
   
+  ## Initialize "extracted" for the std and mean column data and "cols" for the names of those columns
   extracted<-NULL
   cols<-NULL
   
+  ## The limit of the for loop is the number of features
   col_cnt<-dim(features)[1]
 
-  ## Build up the matrix (extracted) with only the std and mean columns, plus the correct column names (cols)  
+  ## Build up the "extracted" matrix with only the std and mean columns, plus the correct column names "cols"  
   for(i in 1:col_cnt){
   if((grepl("mean",features[i,2]) || grepl("std",features[i,2])) && !grepl("meanFreq",features[i,2])){
     extracted<-cbind(extracted,merged[,i+2])
@@ -45,20 +53,29 @@ run_analysis <- function(){
   extracted<-cbind(merged[,1:2],extracted)
   
   ## Add the "subj" and "act" names to the cols vector
-  cols<-c("subj","act",cols)
+  cols<-c("subject","act",cols)
   
   ## Name the columns correctly using the built up column vector
   colnames(extracted)<-cols
   
-  ## Join the activ.labels and extracted tables to get accociate the activity names with the numeric key
-  res2<-merge(activ.labels,extracted)
+  ## Join the activ.labels and extracted tables on "act" to associate the activity names with the numeric key
+  result<-merge(activ.labels,extracted)
   
-  ## Remove the now superfluous act, that is number column
-  res3<-select(res2,-act)
+  ## Remove the now superfluous act, which the tables above were joined on
+  result<-select(result,-act)
   
-  ## Tidy the data
+  browser()
   
-  ## Return averages for each variable by activity and subject
+  ## Tidy the data and calculate the averages for each variable by activity and subject
+  means = aggregate(result, by=list(activity=result$activity, subject=result$subject), mean)
   
+  ## remove extraneous rows from aggregate
+  means[,3] <- NULL
+  means[,3] <- NULL
 
+  ## Write the result to a file
+  write.table(means, "means.txt", sep="\t", row.names = FALSE)
+  
+  ## Return the result
+  means
 }
