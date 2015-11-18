efa68a9d9156a74c03853e19e8f1b1f28f03431b
tbudd <tblsit@gmail.com>
2015-11-17 19:43:33 -0500

    Just need to sort, group by, average on, etc

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,64 @@
 run_analysis <- function(){
+  ## this function assumes all of the files are in .\UCI HAR Dataset in the original file heirarchy
+  ## For field and file descriptions refer to the run_analysis code book.
+  
   ## necessary libraries
   library("dplyr")
 
   ## Read in data files and name the columns human readibly
   activ.labels<-tbl_df(read.table("UCI HAR Dataset\\activity_labels.txt",stringsAsFactors = FALSE))
-  activ.labels<-select(activ.labels,ndx=V1,name=V2)
-
+  activ.labels<-select(activ.labels,act=V1,activity=V2)
+  
   features<-tbl_df(read.table("UCI HAR Dataset\\features.txt",stringsAsFactors = FALSE))
   features<-select(features,ndx=V1,name=V2)
 
-  subj.train<-tbl_df(read.table("UCI HAR Dataset\\train\\subject_train.txt"))
-  select(subj.train,subj=V1)
-
-  y.train<-tbl_df(read.table("UCI HAR Dataset\\train\\y_train.txt"))
-
+  ## Read in the training files. Prepend the data values with the subjects and actvities
+  subj.train<-read.table("UCI HAR Dataset\\train\\subject_train.txt")
+  y.train<-read.table("UCI HAR Dataset\\train\\y_train.txt")
   x.train<-read.table("UCI HAR Dataset\\train\\x_train.txt")
-  colnames(x.train)<-features$name
+  train<-cbind(subj.train,y.train,x.train)
+  
+  ## Read in the test files. Prepend the data values with the subjects and actvities
+  subj.test<-read.table("UCI HAR Dataset\\test\\subject_test.txt")
+  y.test<-read.table("UCI HAR Dataset\\test\\y_test.txt")
+  x.test<-read.table("UCI HAR Dataset\\test\\x_test.txt")
+  test<-cbind(subj.test,y.test,x.test)
+  
+  ## Append the test observations to the training observations
+  merged<-rbind(train,test)
+  
+  extracted<-NULL
+  cols<-NULL
+  
+  col_cnt<-dim(features)[1]
+
+  ## Build up the matrix (extracted) with only the std and mean columns, plus the correct column names (cols)  
+  for(i in 1:col_cnt){
+  if((grepl("mean",features[i,2]) || grepl("std",features[i,2])) && !grepl("meanFreq",features[i,2])){
+    extracted<-cbind(extracted,merged[,i+2])
+    cols<-c(cols,features[i,2])
+  }
+    i<-i+1
+  }
+
+  ## Add back on the subject and activity columns at the front
+  extracted<-cbind(merged[,1:2],extracted)
+  
+  ## Add the "subj" and "act" names to the cols vector
+  cols<-c("subj","act",cols)
+  
+  ## Name the columns correctly using the built up column vector
+  colnames(extracted)<-cols
+  
+  ## Join the activ.labels and extracted tables to get accociate the activity names with the numeric key
+  res2<-merge(activ.labels,extracted)
+  
+  ## Remove the now superfluous act, that is number column
+  res3<-select(res2,-act)
+  
+  ## Tidy the data
+  
+  ## Return averages for each variable by activity and subject
+  
 
 }
-
-## use contains
